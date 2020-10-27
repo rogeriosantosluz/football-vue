7a7a2fef68cbd8870939ae4d55ebfcb50d01c2d3
Rogerio Luz <rogerioluz@MacBook-Pro-de-Rogerio.local>
2020-10-27 00:10:52 -0300

    Primeiro commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Footaball Championship é uma prova de conceito de Vue.js e Flask</h1>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,129 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <table class="left">
+        <h1>Championships</h1>
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(championship) in championships" :key="championship.id"
+              :class="{'highlight': (championship.id == selectedChampionship)}"
+              v-on:click="getTeams(championship)">
+              <td align="left">{{ championship.name}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col">
+        <table class="left">
+        <h1>Teams</h1>
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(team) in teams" :key="team.id"
+              :class="{'highlight': (team.id == selectedTeam)}"
+              v-on:click="getPlayers(team)">
+              <td align="left">{{ team.name}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col">
+        <table class="left">
+        <h1>Players</h1>
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(player) in players" :key="player.id">
+              <td align="left">
+              {{ player.number}}-{{ player.name}} ({{ player.position}}) {{ player.nation}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      championships: [],
+      selectedChampionship: '',
+      teams: [],
+      selectedTeam: '',
+      players: [],
+    };
+  },
+  methods: {
+    getPlayers(team) {
+      this.selectedTeam = team.id;
+      const path = 'http://localhost:5000/players';
+      axios.get(path, {
+        params: {
+          team: team.id,
+        },
+      })
+        .then((res) => {
+          this.players = res.data.players;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    getTeams(championship) {
+      this.selectedChampionship = championship.id;
+      const path = 'http://localhost:5000/teams';
+      axios.get(path, {
+        params: {
+          championship: championship.id,
+        },
+      })
+        .then((res) => {
+          this.teams = res.data.teams;
+          this.players = [];
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    getChampionships() {
+      const path = 'http://localhost:5000/championships';
+      axios.get(path)
+        .then((res) => {
+          this.championships = res.data.championships;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getChampionships();
   },
 };
 </script>
+<style>
+.highlight {
+     background-color: yellow;
+}
+tr:hover{
+     cursor: pointer;
+}
+</style>
